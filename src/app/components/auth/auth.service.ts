057a126249901cbdc790cd154036cfb65a613a06
Ramprit sahani <Rampritsahani@outlook.in>
2017-10-31 22:22:10 +0530

    lodash added

--- src/app/components/auth/auth.service.ts
+++ src/app/components/auth/auth.service.ts
@@ -3,12 +3,31 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+import * as _ from 'lodash';
+import { AngularFirestoreCollection, AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+
+interface User {
+  uid: string;
+  email: string;
+  photoUrl?: string;
+  favoriteColor?: string;
+}
 
 @Injectable()
 export class AuthService {
+  user: Observable<User>;
   user$: Observable<firebase.User>;
-  constructor(private router: Router, public authService: AngularFireAuth) {
+  constructor(private router: Router,
+    public authService: AngularFireAuth,
+    public afs: AngularFirestore) {
     this.user$ = this.authService.authState;
+    this.user = this.authService.authState.switchMap(user => {
+      if (user) {
+        return this.afs.doc<User>(`users/${user.uid}`).valueChanges()
+      } else
+        return Observable.of(null);
+    });
   }
 
   login() {
