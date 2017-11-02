850f4f956f4f5a702096df67a4959c01625bd50c
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-02 15:46:35 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { MenuItem, MenuModule } from 'primeng/primeng';
+import { Menu } from 'primeng/components/menu/menu';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+declare var jQuery: any;
 
 @Component({
   selector: 'b-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'b';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  menuItems: MenuItem[];
+  miniMenuItems: MenuItem[];
+
+  @ViewChild('bigMenu') bigMenu: Menu;
+  @ViewChild('smallMenu') smallMenu: Menu;
+  menuVisible = false;
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+
+    const handleSelected = function (event) {
+      const allMenus = jQuery(event.originalEvent.target).closest('ul');
+      const allLinks = allMenus.find('.menu-selected');
+
+      allLinks.removeClass('menu-selected');
+      const selected = jQuery(event.originalEvent.target).closest('a');
+      selected.addClass('menu-selected');
+    };
+
+    this.menuItems = [
+      // tslint:disable-next-line:max-line-length
+      { label: 'Dashboard', icon: 'fa-home', routerLink: ['/dashboard'], command: (event) => handleSelected(event), visible: !this.menuVisible },
+      { label: 'All Times', icon: 'fa-calendar', routerLink: ['/alltimes'], command: (event) => handleSelected(event), visible: this.menuVisible },
+      // tslint:disable-next-line:max-line-length
+      { label: 'Fruit', icon: 'fa-clock-o', routerLink: ['/fruits'], command: (event) => handleSelected(event), visible: !this.menuVisible },
+      { label: 'Add Project', icon: 'fa-tasks', routerLink: ['/projects'], command: (event) => handleSelected(event), visible: this.menuVisible },
+      // tslint:disable-next-line:max-line-length
+      { label: 'Employee', icon: 'fa-users', routerLink: ['/employees'], command: (event) => handleSelected(event), visible: !this.menuVisible },
+      { label: 'Settings', icon: 'fa-sliders', routerLink: ['/settings'], command: (event) => handleSelected(event), visible: this.menuVisible },
+      // tslint:disable-next-line:max-line-length
+      { label: 'Product', icon: 'fa-empire', routerLink: ['/products'], command: (event) => handleSelected(event), visible: this.menuVisible },
+      { label: 'Google Login', icon: 'fa-google', routerLink: ['/products'], command: (event) => handleSelected(event), visible: this.menuVisible },
+    ];
+
+    this.miniMenuItems = [];
+    this.menuItems.forEach((item: MenuItem) => {
+      const miniItem = { icon: item.icon, routerLink: item.routerLink }
+      this.miniMenuItems.push(miniItem);
+    });
+
+  }
+
+  selectInitialMenuItemBasedOnUrl() {
+    const path = document.location.pathname;
+    const menuItem = this.menuItems.find((item) => item.routerLink[0] === path);
+    if (menuItem) {
+      const selectedIcon = this.bigMenu.container.querySelector(`.${menuItem.icon}`);
+      jQuery(selectedIcon).closest('li').addClass('menu-selected');
+    }
+  }
+
+  ngAfterViewInit() {
+    this.selectInitialMenuItemBasedOnUrl();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,85 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MenuModule } from 'primeng/primeng';
+import { PanelModule } from 'primeng/primeng';
+import { ChartModule } from 'primeng/primeng';
+import { InputTextModule } from 'primeng/primeng';
+import { ButtonModule } from 'primeng/primeng';
+import { InputMaskModule } from 'primeng/primeng';
+import { InputTextareaModule } from 'primeng/primeng';
+import { EditorModule } from 'primeng/primeng';
+import { CalendarModule } from 'primeng/primeng';
+import { RadioButtonModule } from 'primeng/primeng';
+import { FieldsetModule } from 'primeng/primeng';
+import { DropdownModule } from 'primeng/primeng';
+import { MultiSelectModule } from 'primeng/primeng';
+import { ListboxModule } from 'primeng/primeng';
+import { SpinnerModule } from 'primeng/primeng';
+import { SliderModule } from 'primeng/primeng';
+import { RatingModule } from 'primeng/primeng';
+import { DataTableModule } from 'primeng/primeng';
+import { ContextMenuModule } from 'primeng/primeng';
+import { TabViewModule } from 'primeng/primeng';
+import { DialogModule } from 'primeng/primeng';
+import { StepsModule } from 'primeng/primeng';
+import { ScheduleModule } from 'primeng/primeng';
+import { TreeModule } from 'primeng/primeng';
+import { GMapModule } from 'primeng/primeng';
+import { DataGridModule } from 'primeng/primeng';
+import { TooltipModule } from 'primeng/primeng';
+import { ConfirmationService } from 'primeng/primeng';
+import { ConfirmDialogModule } from 'primeng/primeng';
+import { GrowlModule } from 'primeng/primeng';
+import { DragDropModule } from 'primeng/primeng';
+import { GalleriaModule } from 'primeng/primeng';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MenuModule,
+    PanelModule,
+    ChartModule,
+    InputTextModule,
+    ButtonModule,
+    InputMaskModule,
+    InputTextareaModule,
+    EditorModule,
+    CalendarModule,
+    RadioButtonModule,
+    FieldsetModule,
+    DropdownModule,
+    MultiSelectModule,
+    ListboxModule,
+    SpinnerModule,
+    SliderModule,
+    RatingModule,
+    DataTableModule,
+    ContextMenuModule,
+    TabViewModule,
+    DialogModule,
+    StepsModule,
+    ScheduleModule,
+    TreeModule,
+    GMapModule,
+    DataGridModule,
+    TooltipModule,
+    ConfirmDialogModule,
+    GrowlModule,
+    DragDropModule,
+    GalleriaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
