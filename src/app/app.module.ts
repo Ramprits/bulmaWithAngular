a1068d13df1951e66ff187daf4f676d403967223
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-03 14:41:04 +0530

    modified all import in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,53 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
-import { MenuModule } from 'primeng/primeng';
-import { PanelModule } from 'primeng/primeng';
-import { ChartModule } from 'primeng/primeng';
-import { InputTextModule } from 'primeng/primeng';
-import { ButtonModule } from 'primeng/primeng';
-import { InputMaskModule } from 'primeng/primeng';
-import { InputTextareaModule } from 'primeng/primeng';
-import { EditorModule } from 'primeng/primeng';
-import { CalendarModule } from 'primeng/primeng';
-import { RadioButtonModule } from 'primeng/primeng';
-import { FieldsetModule } from 'primeng/primeng';
-import { DropdownModule } from 'primeng/primeng';
-import { MultiSelectModule } from 'primeng/primeng';
-import { ListboxModule } from 'primeng/primeng';
-import { SpinnerModule } from 'primeng/primeng';
-import { SliderModule } from 'primeng/primeng';
-import { RatingModule } from 'primeng/primeng';
-import { DataTableModule } from 'primeng/primeng';
-import { ContextMenuModule } from 'primeng/primeng';
-import { TabViewModule } from 'primeng/primeng';
-import { DialogModule } from 'primeng/primeng';
-import { StepsModule } from 'primeng/primeng';
-import { ScheduleModule } from 'primeng/primeng';
-import { TreeModule } from 'primeng/primeng';
-import { GMapModule } from 'primeng/primeng';
-import { DataGridModule } from 'primeng/primeng';
-import { TooltipModule } from 'primeng/primeng';
-import { ConfirmationService } from 'primeng/primeng';
-import { ConfirmDialogModule } from 'primeng/primeng';
-import { GrowlModule } from 'primeng/primeng';
-import { DragDropModule } from 'primeng/primeng';
-import { GalleriaModule } from 'primeng/primeng';
+// tslint:disable-next-line:max-line-length
+import { HomeComponent, BookComponent, BookService, DashboardComponent, CustomerComponent, CustomerService, CustomerDetailComponent } from './components/index';
+// tslint:disable-next-line:max-line-length
+import { MenuModule, PanelModule, ChartModule, InputTextModule, ButtonModule, InputMaskModule, InputTextareaModule, EditorModule, CalendarModule, RadioButtonModule, FieldsetModule, DropdownModule, MultiSelectModule, ListboxModule, SpinnerModule, SliderModule, RatingModule, DataTableModule, ContextMenuModule, TabViewModule, DialogModule, StepsModule, ScheduleModule, TreeModule, GMapModule, DataGridModule, TooltipModule, ConfirmationService, ConfirmDialogModule, GrowlModule, DragDropModule, GalleriaModule } from 'primeng/primeng';
 
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BookComponent } from './components/book/book.component';
-import { BookService } from './components/book/book.service';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment.prod';
 import { FielderrorsComponent } from './core/fielderrors/fielderrors.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { CustomerComponent } from './components/customer/customer.component';
-import { CustomerService } from './components/customer/customer.service';
-import { CustomerDetailComponent } from './components/customer/customer-detail/customer-detail.component';
 
 
 @NgModule({
---
+++ src/app/components/index.ts
@@ -0,0 +1,10 @@
+// tslint:disable-next-line:eofline
+export * from '../components/book/book.component';
+export * from '../components/book/book.service';
+export * from '../components/customer/customer.service';
+export * from '../components/book/book';
+export * from '../components/customer/customer.component';
+export * from '../components/customer/customer-detail/customer-detail.component';
+export * from '../components/customer/customer';
+export * from '../components/dashboard/dashboard.component';
+export * from '../components/home/home.component';
