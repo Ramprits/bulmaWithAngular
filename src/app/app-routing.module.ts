620c71c3888417a75687aef30c6317130e4f9527
R1163213 <Ramprit.Sahani@ext.saint-gobain.com>
2017-11-10 16:40:52 +0530

    add employee form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// tslint:disable-next-line:max-line-length
-import { HomeComponent, BookComponent, CustomerComponent, CustomerDetailComponent, DashboardComponent, FruitComponent, AddCustomerComponent, EmployeeComponent } from './components/index';
+import {
+  HomeComponent, BookComponent, CustomerComponent, CustomerDetailComponent,
+  DashboardComponent, FruitComponent, AddCustomerComponent, EmployeeComponent
+} from './components/index';
 import { AuthorComponent } from './components/author/author.component';
+import { AddEmployeeComponent } from './components/employee/add-employee/add-employee.component';
 
 const routes: Routes = [
 
@@ -15,6 +18,7 @@ const routes: Routes = [
   { path: 'customers/:customerId', component: CustomerDetailComponent },
   { path: 'fruits', component: FruitComponent },
   { path: 'authors', component: AuthorComponent },
+  { path: 'addEmployee', component: AddEmployeeComponent },
   { path: 'employees', component: EmployeeComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' }
 
--- src/app/components/employee/add-employee/add-employee.component.ts
+++ src/app/components/employee/add-employee/add-employee.component.ts
@@ -1,29 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SelectItem } from 'primeng/components/common/api';
 
 @Component({
   selector: 'b-add-employee',
   templateUrl: './add-employee.component.html'
 })
 export class AddEmployeeComponent implements OnInit {
-  AddEmployee: FormGroup;
+  addEmployee: FormGroup;
+  genders: SelectItem[];
   constructor(private fb: FormBuilder) { }
-  employee = Employee;
+  employee: Employee = {
+    firstName: '',
+    lastName: '', email: '',
+    mobile: '', gender: '', address: ''
+  };
   ngOnInit() {
-    this.AddEmployee = this.fb.group({
-      firstName: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
-      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      email: ['', [Validators.required]],
-      mobile: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
-      gender: ['', [Validators.required]],
-      address: ['', [Validators.required, Validators.minLength(20), Validators.maxLength(200)]]
+    this.addEmployee = this.fb.group({
+      firstName: [this.employee.firstName, [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+      lastName: [this.employee.lastName, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: [this.employee.email, [Validators.required]],
+      mobile: [this.employee.mobile, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      gender: [this.employee.gender, [Validators.required]],
+      address: [this.employee.address, [Validators.required, Validators.minLength(20), Validators.maxLength(200)]]
     });
+    this.genders = [];
+    this.genders.push({ label: 'Select Gender', value: '' });
+    this.genders.push({ label: 'Male', value: 'Male' });
+    this.genders.push({ label: 'Female', value: 'Female' });
   }
+  onSubmit(form) {
 
+  }
 }
 export class Employee {
-  constructor(public firstName?: '',
-    public lastName?: '', public email?: '',
-    public mobile?: '', public gender?: '', public address?: '') {
-  }
+  public firstName?: '';
+  public lastName?: '';
+  public email?: '';
+  public mobile?: '';
+  public gender?: '';
+  public address?: '';
 }
