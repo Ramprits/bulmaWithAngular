c595effed39ffbe976998b7dfc763d1f6542ba49
Ramprit sahani <Rampritsahani@outlook.in>
2017-11-11 14:02:13 +0530

    Employee with add

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -2,19 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from './employee.service';
 import { IEmployee } from './employee';
 import { TrackerError } from '../../core/index';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'b-employee',
-  templateUrl: './employee.component.html'
+  templateUrl: './employee.component.html',
+  styles: [`p-dataTable >>> .ui-datatable-footer {
+  min-height: 60px;
+}`]
 })
 export class EmployeeComponent implements OnInit {
   employees: IEmployee[] | TrackerError;
   selectedEmployee: IEmployee;
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService, private router: Router) { }
   ngOnInit() {
     this.employeeService.getEmployees().subscribe((employee: IEmployee[]) => {
       this.employees = employee;
     });
   }
-
+  addEmployee() {
+    this.router.navigate(['/addEmployee']);
+  }
 }
